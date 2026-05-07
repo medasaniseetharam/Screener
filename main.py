3f597bfe0a1bb57099818b7aeea09674e278be15
medasaniseetharam <medasani.seetharam@gmail.com>
2026-05-07 19:44:10 +0530

    Refactor main.py for clarity and structure

--- main.py
+++ main.py
@@ -136,10 +136,10 @@ print("🚀 Cloud Scanner Started...")
 
 # schedule.every(1).minutes.do(run_scan)  # Test mode
 
-schedule.every().day.at("12:35").do(run_scan)
+# schedule.every().day.at("12:35").do(run_scan)
 
 # Run once immediately
-run_scan()
+# run_scan()
 
 # Keep alive
 while True:
